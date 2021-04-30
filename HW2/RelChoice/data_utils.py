6d205e85475b53c68a1512b512f717d805974d07
joe0123 <boewoei0123@gmail.com>
2021-04-30 22:09:17 +0800

    backup for relchoice

--- HW2/RelChoice/data_utils.py
+++ HW2/RelChoice/data_utils.py
@@ -1,17 +1,18 @@
 def prepare_train_features(examples, args, tokenizer):
     pad_on_right = (tokenizer.padding_side == "right")
-    first_sentences, second_sentences, sentence_counts = [], [], []
-    for ques, paras in zip(examples[args.ques_col], examples[args.para_col]):
-        if len(paras) < 5:
-            paras += ['' for i in range(5 - len(paras))]
-        for p in paras:
+    first_sentences, second_sentences, sentence_counts, labels = [], [], [], []
+    for ques, paras, label in zip(examples[args.ques_col], examples[args.para_col], examples[args.label_col]):
+        if len(paras) < args.neg_num + 1:
+            paras += ['' for i in range(args.neg_num + 1 - len(paras))]
+        sentence_counts.append(len(paras))
+        labels.append(0)    # For easier negative sampling implementation in collator.
+        for i in [label] + range(0, label) + range(label + 1, len(params)):
             if pad_on_right:
                 first_sentences.append(ques)
-                second_sentences.append(p)
+                second_sentences.append(paras[i])
             else:
-                first_sentences.append(p)
+                first_sentences.append(paras[i])
                 second_sentences.append(ques)
-        sentence_counts.append(len(paras))
         
     max_seq_len = min(args.max_seq_len, tokenizer.model_max_length)
     tokenized_inputs = tokenizer(
@@ -30,7 +31,7 @@ def prepare_train_features(examples, args, tokenizer):
             tokenized_examples[k].append(v[curr: curr + c])
             curr += c
         assert curr == len(v)
-    tokenized_examples["labels"] = examples[args.label_col]
+    tokenized_examples["labels"] = labels
     
     return tokenized_examples
 
@@ -61,6 +62,31 @@ def prepare_pred_features(examples, args, tokenizer):
     return tokenized_examples
 
 
-def data_collator_with_neg_sampling():
+def data_collator_with_neg_sampling(features, args):
+    if not isinstance(features[0], (dict, BatchEncoding)):
+        features = [vars(f) for f in features]
+
+    first = features[0]
+    batch = {}
+
+    if "label" in first and first["label"] is not None:
+        label = first["label"].item() if isinstance(first["label"], torch.Tensor) else first["label"]
+        dtype = torch.long if isinstance(label, int) else torch.float
+        batch["labels"] = torch.tensor([f["label"] for f in features], dtype=dtype)
+    elif "label_ids" in first and first["label_ids"] is not None:
+        if isinstance(first["label_ids"], torch.Tensor):
+            batch["labels"] = torch.stack([f["label_ids"] for f in features])
+        else:
+            dtype = torch.long if type(first["label_ids"][0]) is int else torch.float
+            batch["labels"] = torch.tensor([f["label_ids"] for f in features], dtype=dtype)
+
+    for k, v in first.items():
+        if k not in ("label", "label_ids") and v is not None and not isinstance(v, str):
+            if isinstance(v, torch.Tensor):
+                batch[k] = torch.stack([f[k] for f in features])
+            else:
+                batch[k] = torch.tensor([f[k] for f in features])
+
+    return batch
 
 
